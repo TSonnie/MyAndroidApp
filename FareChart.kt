8b1a80e1e093037f433ac4cae38f66b452d4786e
TSonnie <31431451+TSonnie@users.noreply.github.com>
2018-10-28 18:10:00 +0300

    Add files via upload

---
+++ FareChart.kt
@@ -0,0 +1,12 @@
+package com.example.muthoni.mybusride
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class FareChart : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fare_chart)
+    }
+}
